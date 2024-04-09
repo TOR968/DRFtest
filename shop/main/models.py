1deac91de1173d16fc7e5e628863eb19231dc513
Maxim Turbal <turbalmaxim@gmail.com>
2024-04-09 14:54:21 +0300

    Initial commit

---
+++ shop/main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_available = models.IntegerField(default=0)
+    image = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f'Name: {self.name} price: {self.price}'
